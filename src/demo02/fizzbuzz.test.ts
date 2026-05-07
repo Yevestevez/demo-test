4e922d24b2e2a7ab71e0d65b5d0d6b3042de9ceb
Eduardo Yeves <yevestevez.cg@gmail.com>
2026-05-07 12:29:29 +0100

    Refactor fizzBuzz function logic and enhance test coverage for edge cases

--- src/demo02/fizzbuzz.test.ts
+++ src/demo02/fizzbuzz.test.ts
@@ -1,6 +1,18 @@
 import { fizzBuzz } from './fizzbuzz.ts';
 
 describe('Given FizzBuzz function', () => {
+    describe('When it receives 1', () => {
+        test('Then it should return 1', () => {
+            // Arrange
+            const input = 1;
+            const expectedOutput = '1';
+            // Act
+            const result = fizzBuzz(input);
+            // Assert
+            expect(result).toBe(expectedOutput);
+        });
+    });
+
     describe('When it receives 3', () => {
         test('Then it should return Fizz', () => {
             // Arrange
@@ -25,6 +37,42 @@ describe('Given FizzBuzz function', () => {
         });
     });
 
+    describe('When it receives 7', () => {
+        test('Then it should return 7', () => {
+            // Arrange
+            const input = 7;
+            const expectedOutput = '7';
+            // Act
+            const result = fizzBuzz(input);
+            // Assert
+            expect(result).toBe(expectedOutput);
+        });
+    });
+
+    describe('When it receives 9', () => {
+        test('Then it should return Fizz', () => {
+            // Arrange
+            const input = 9;
+            const expectedOutput = 'Fizz';
+            // Act
+            const result = fizzBuzz(input);
+            // Assert
+            expect(result).toBe(expectedOutput);
+        });
+    });
+
+    describe('When it receives 10', () => {
+        test('Then it should return Buzz', () => {
+            // Arrange
+            const input = 10;
+            const expectedOutput = 'Buzz';
+            // Act
+            const result = fizzBuzz(input);
+            // Assert
+            expect(result).toBe(expectedOutput);
+        });
+    });
+
     describe('When it receives 15', () => {
         test('Then it should return FizzBuzz', () => {
             // Arrange
@@ -37,11 +85,11 @@ describe('Given FizzBuzz function', () => {
         });
     });
 
-    describe('When it receives 1', () => {
-        test('Then it should return 1', () => {
+    describe('When it receives 30', () => {
+        test('Then it should return FizzBuzz', () => {
             // Arrange
-            const input = 1;
-            const expectedOutput = '1';
+            const input = 30;
+            const expectedOutput = 'FizzBuzz';
             // Act
             const result = fizzBuzz(input);
             // Assert
--- src/demo02/fizzbuzz.ts
+++ src/demo02/fizzbuzz.ts
@@ -1,14 +1,14 @@
-// Ejercicio FizzBuzz -> Imprime los números del 1 al 100, pero para múltiplos de 3 imprime "Fizz", para múltiplos de 5 imprime "Buzz" y para múltiplos de ambos imprime "FizzBuzz".
+// Ejercicio FizzBuzz -> Imprime los números del 1 al 100, pero para múltiplos de 3 imprime "Fizz", para múltiplos de 5 imprime "Buzz" y para múltiplos de ambos imprime "FizzBuzz". Si no son múltiplos de 3 ni de 5 debe devolver el número introducido en string
 
 export const fizzBuzz = (n: number): string => {
     const responses: [string, string, string] = ['Fizz', 'Buzz', 'FizzBuzz'];
 
-    if (n === 3) {
-        return responses[0];
-    } else if (n === 5) {
-        return responses[1];
-    } else if (n === 3 * 5) {
+    if (n % 3 === 0 && n % 5 === 0) {
         return responses[2];
+    } else if (n % 3 === 0) {
+        return responses[0];
+    } else if (n % 5 === 0) {
+        return responses[1];
     } else {
         return n.toString();
     }
